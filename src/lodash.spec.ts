807b39cd45e5602fcc658e533bb2c0e080520349
안운장 <hodol@woowahan.com>
2021-01-30 18:23:16 +0900

    feat: 전개연산자 테스트 추가

--- src/lodash.spec.ts
+++ src/lodash.spec.ts
@@ -21,4 +21,23 @@ describe('프로필을 만들고', () => {
       ],
     };
   });
+
+  describe('전개연산자로 복사하면', () => {
+    let hodolClone: Profile;
+    beforeEach(() => {
+      hodolClone = { ...hodol };
+    });
+
+    it('name을 바꿔도 원본은 변하지 않는다.', () => {
+      hodolClone.name = 'roonjang';
+      expect(hodolClone.name).toBe('roonjang');
+      expect(hodol.name).toBe('woonjang');
+    });
+
+    it('teammates의 name을 바꾸면 원본도 변한다.', () => {
+      hodolClone.teammates[0].name = 'siyoung';
+      expect(hodolClone.teammates[0].name).toBe('siyoung');
+      expect(hodol.teammates[0].name).toBe('siyoung');
+    });
+  });
 });
